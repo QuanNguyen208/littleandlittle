086876d6ff9ce6abc6cbe00a8e968982d544fa3b
unknown <you@example.com>
2022-07-14 15:44:44 +0700

    home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,642 +6,38 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="stylesheet1.css">
+  <link rel="icon" href="./favicon.ico" type="image/x-icon">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
 </head>
-<style>
-  li {
-  float: left;
-  }
-  .navigation{
-    position: absolute;
-    width: 1924.5px;
-    height: 122.46px;
-    left: 0px;
-    top: 0px;
-  }
-  .container-fluid{
-    position: absolute;
-    width: 1924.5px;
-    height: 107.82px;
-    left: 0px;
-    top: 0px;
 
-    background: #ED7200;
-    box-shadow: 0px 4px 35px rgba(162, 58, 0, 0.75);
-  }
-  .navbar-brand{
-    position: absolute;
-    width: 229px;
-    height: 91px;
-    left: 201px;
-    top: 1px;
-  }
-  .Group2{
-  }
-  .homeBox{
-    box-sizing: border-box;
-
-/* Auto layout */
-
-    display: flex;
-    flex-direction: row;
-    align-items: flex-start;
-    padding: 10px 24px;
-    gap: 10px;
-
-    width: 144px;
-    height: 42px;
-
-    /* W */
-
-    border: 2px solid #FFFFFF;
-    border-radius: 33px;
-
-    /* Inside auto layout */
-
-    flex: none;
-    order: 0;
-    flex-grow: 0;}
-  li {
-    display: flex;
-    flex-direction: row;
-    align-items: flex-start;
-    padding: 10px 24px;
-    gap: 10px;
-
-    width: 144px;
-    height: 42px;
-
-    /* W */
-
-    border-radius: 33px;
-
-    /* Inside auto layout */
-
-    flex: none;
-    order: 0;
-    flex-grow: 0;
-  }
-  li a {
-    width: 96px;
-    height: 22px;
-
-    font-family: 'Montserrat';
-    font-style: normal;
-    font-weight: 700;
-    font-size: 18px;
-    line-height: 22px;
-    /* identical to box height */
-
-
-    /* W */
-
-    color: #FFFFFF;
-
-
-    /* Inside auto layout */
-
-    flex: none;
-    order: 0;
-    flex-grow: 0;
-  }
-
-  .Frame20{
-    display: flex;
-    flex-direction: row;
-    align-items: flex-start;
-    padding: 0px;
-    gap: 80px;
-
-    position: absolute;
-    width: 541px;
-    height: 42px;
-    left: 703px;
-    top: 29px;
-
-  }
-
-  .Frame21{
-    box-sizing: border-box;
-
-    position: absolute;
-    width: 32px;
-    height: 32px;
-    left: 0px;
-    top: 0px;
-
-    /* W */
-
-    border: 1px solid #FFFFFF;
-    border-radius: 16px;
-  }
-
-  .Frame22{
-    /* Frame 22 */
-
-
-/* Auto layout */
-
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    padding: 0px;
-    gap: 8px;
-
-    position: absolute;
-    width: 137px;
-    height: 32px;
-    left: 1456px;
-    top: 34px;
-  }
-
-  .Group{
-    /* Group */
-
-
-    width: 32px;
-    height: 32px;
-
-
-    /* Inside auto layout */
-
-    flex: none;
-    order: 0;
-    flex-grow: 0;
-  }
-  .phoneNumber{
-    width: 97px;
-    height: 20px;
-
-    /* Montserrat / 16 bold */
-
-    font-family: 'Montserrat';
-    font-style: normal;
-    font-weight: 700;
-    font-size: 16px;
-    line-height: 20px;
-    /* identical to box height */
-
-
-    /* W */
-
-    color: #FFFFFF;
-
-
-    /* Inside auto layout */
-
-    flex: none;
-    order: 1;
-    flex-grow: 0;
-  }
-  .mainBG{
-    position: absolute;
-    width: 1784px;
-    height: 961px;
-    left: 75px;
-    top: 70px;
-
-    /* Gradient / bg */
-
-    background: linear-gradient(180deg, #FFB648 0%, #FFB648 100%);
-    box-shadow: 0px 4px 27px rgba(180, 106, 38, 0.25);
-    border-radius: 0px 0px 56px 56px;
-  }
-  .mainFrame{
-    position: absolute;
-    width: 1920px;
-    height: 1104px;
-    left: 0px;
-    top: 0px;
-
-  }
-  .Lisa{
-    position: absolute;
-    width: 488px;
-    height: 510px;
-    left: -165px;
-    top: 528px;
-
-
-  }
-  .group1{
-    position: absolute;
-    width: 284px;
-    height: 80px;
-    left: 1309px;
-    top: 346px;
-
-    /* button */
-
-    filter: drop-shadow(0px 4px 8px rgba(165, 15, 15, 0.45));
-  }
-  .yourticket{
-    position: absolute;
-    left: 71.36%;
-    right: 20.04%;
-    top: 32.43%;
-    bottom: 63.86%;
-
-    /* Koni / 28 */
-
-    font-family: 'iCiel Koni';
-    font-style: normal;
-    font-weight: 900;
-    font-size: 28px;
-    line-height: 41px;
-
-    /* W */
-
-    color: #FFFFFF;
-
-  }
-  p.DamSen{
-    position: absolute;
-    width: 322px;
-    height: 100px;
-    left: 397px;
-    top: 137px;
-
-    font-family: 'iCiel Koni';
-    font-style: normal;
-    font-weight: 900;
-    font-size: 72px;
-    line-height: 100px;
-    /* identical to box height, or 139% */
-
-
-    /* W */
-
-    color: #FFFFFF;
-
-  }
-  p.Park{
-    position: absolute;
-    width: 197px;
-    height: 100px;
-    left: 402px;
-    top: 213px;
-
-    font-family: 'iCiel Koni';
-    font-style: normal;
-    font-weight: 900;
-    font-size: 72px;
-    line-height: 100px;
-    /* identical to box height, or 139% */
-
-
-    /* W */
-
-    color: #FFFFFF;
-  }
-  .image2{
-    position: absolute;
-    left: 10.42%;
-    right: 80.16%;
-    top: 14.22%;
-    bottom: 72.64%;
-
-  }
-  .renderfixhair1{
-    position: absolute;
-    width: 788px;
-    height: 443px;
-    left: 1052px;
-    top: 23px;
-
-  }
-  .balloon6{
-    position: absolute;
-    width: 147.37px;
-    height: 210px;
-    left: 18px;
-    top: 321px;
-
-  }
-  .balloon5{
-    position: absolute;
-    left: 91.67%;
-    right: -1.98%;
-    top: 34.15%;
-    bottom: 40.22%;
-
-    background: url(18451 [Converted]-05.png);
-  }
-  .balloon4{
-    position: absolute;
-    left: 32.81%;
-    right: 61.46%;
-    top: 84.78%;
-    bottom: 1%;
-
-    background: url(18451 [Converted]-04.png);
-  }
-  .balloon3-2{
-    position: absolute;
-    left: 88.18%;
-    right: 3.18%;
-    top: 74.82%;
-    bottom: 3.71%;
-
-    background: url(18451 [Converted]-03.png);
-  }
-  .balloon3-1{
-    position: absolute;
-    left: 42.03%;
-    right: 50.47%;
-    top: 16.58%;
-    bottom: 64.86%;
-
-    background: url(18451 [Converted]-03.png);
-  }
-  .balloon2{
-    position: absolute;
-    left: 53.59%;
-    right: 40.31%;
-    top: 8.61%;
-    bottom: 75.63%;
-
-    background: url(18451 [Converted]-02.png);
-  }
-  .FrameTicket{
-    position: absolute;
-    width: 1520px;
-    height: 600px;
-    left: 201px;
-    top: 361px;}
-  .FrameDatve{
-    position: absolute;
-    left: 70.2%;
-    right: 5.59%;
-    top: 80.83%;
-    bottom: 7.83%;
-
-    /* button */
-
-    filter: drop-shadow(0px 4px 8px rgba(165, 15, 15, 0.45));
-  }
-  .FrameInfo{
-    position: absolute;
-    left: 66.84%;
-    right: 2.43%;
-    top: 14.5%;
-    bottom: 23.17%;
-  }
-  .Frame5{
-    position: absolute;
-    width: 398px;
-    height: 56px;
-    left: 0px;
-    top: 0px;
-
-    /* W */
-
-    background: #FFFFFF;
-    /* inner shadow */
-
-    box-shadow: inset -1px 3px 3px rgba(179, 91, 11, 0.5);
-    border-radius: 16px;
-  }
-  .Frame6{
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    padding: 15px 16px;
-    gap: 260px;
-
-    position: absolute;
-    width: 150px;
-    height: 56px;
-    left: 0px;
-    top: 72px;
-
-    /* W */
-
-    background: #FFFFFF;
-    /* inner shadow */
-
-    box-shadow: inset -1px 3px 3px rgba(179, 91, 11, 0.5);
-    border-radius: 16px;
-  }
-  .Frame7{
-    position: absolute;
-    width: 224px;
-    height: 56px;
-    left: 174px;
-    top: 72px;
-
-    /* W */
-
-    background: #FFFFFF;
-    /* inner shadow */
-
-    box-shadow: inset -1px 3px 3px rgba(179, 91, 11, 0.5);
-    border-radius: 16px;
-  }
-  .Frame8{
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    padding: 20px 16px;
-    gap: 260px;
-
-    position: absolute;
-    width: 467px;
-    height: 66px;
-    left: 0px;
-    top: 144px;
-
-    /* W */
-
-    background: #FFFFFF;
-    /* inner shadow */
-
-    box-shadow: inset -1px 3px 3px rgba(179, 91, 11, 0.5);
-    border-radius: 16px;
-  }
-  .Frame9{
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    padding: 20px 16px;
-    gap: 260px;
-
-    position: absolute;
-    width: 467px;
-    height: 66px;
-    left: 0px;
-    top: 226px;
-
-    /* W */
-
-    background: #FFFFFF;
-    /* inner shadow */
-
-    box-shadow: inset -1px 3px 3px rgba(179, 91, 11, 0.5);
-    border-radius: 16px;
-  }
-  .Frame10{
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    padding: 20px 16px;
-    gap: 260px;
-
-    position: absolute;
-    width: 467px;
-    height: 66px;
-    left: 0px;
-    top: 308px;
-
-    /* W */
-
-    background: #FFFFFF;
-    /* inner shadow */
-
-    box-shadow: inset -1px 3px 3px rgba(179, 91, 11, 0.5);
-    border-radius: 16px;
-  }
-  .Frame28{
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    padding: 0px;
-    gap: 8px;
-
-    position: absolute;
-    width: 748px;
-    height: 40px;
-    left: 127px;
-    top: 407px;
-  }
-  .Frame27{
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    padding: 0px;
-    gap: 8px;
-
-    position: absolute;
-    width: 748px;
-    height: 40px;
-    left: 127px;
-    top: 351px;
-
-  }
-  .Frame26{
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    padding: 0px;
-    gap: 8px;
-
-    position: absolute;
-    width: 748px;
-    height: 40px;
-    left: 127px;
-    top: 295px;
-
-  }
-  .Frame25{
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    padding: 0px;
-    gap: 8px;
-
-    position: absolute;
-    width: 748px;
-    height: 40px;
-    left: 127px;
-    top: 239px;
-
-  }
-  .LoremParagraph{
-    position: absolute;
-    left: 4.47%;
-    right: 40.92%;
-    top: 8%;
-    bottom: 67%;
-
-    /* 20 medium */
-
-    font-family: 'Montserrat';
-    font-style: normal;
-    font-weight: 500;
-    font-size: 20px;
-    line-height: 30px;
-    /* or 150% */
-
-
-    /* Text */
-
-    color: #23221F;
-
-    opacity: 0.7;
-  }
-  .Textlorem{
-    width: 703px;
-    height: 36px;
-
-    font-family: 'Montserrat';
-    font-style: normal;
-    font-weight: 700;
-    font-size: 24px;
-    line-height: 36px;
-    /* identical to box height, or 150% */
-
-
-    /* Text */
-
-    color: #23221F;
-
-    opacity: 0.8;
-
-    /* Inside auto layout */
-
-    flex: none;
-    order: 1;
-    flex-grow: 0;
-  }
-  .Datve{
-    position: absolute;
-    left: 37.23%;
-    right: 37.23%;
-    top: 8.82%;
-    bottom: 2.94%;
-
-    font-family: 'iCiel Koni';
-    font-style: normal;
-    font-weight: 900;
-    font-size: 28px;
-    line-height: 41px;
-
-    /* W */
-
-    color: #FFFFFF;
-  }
-
-  
-</style>
 <body>
 
 
-  <header class="navigation">
-  <div class="container-fluid">
-      <img src="/images/littlechannel.jpg" class="navbar-brand">
+  <div class="mainBG">
+
+  <div class="groupbg">
+        <img src="/images/bg.png" class="imagebg" >
+  </div>
+
+
+  <nav class="navigation">
+    
+  <div>
+      <img src="/images/Vector 2.png" class="Vector2">
+      <img src="/images/Little & Little Logo (ngang) 1.png" class="navbar-brand">
 
     <div class="Group2">
-      <div class="Frame20">
-        <ul>
+
+      <div>
+        <ul class="Frame20">
           <li class="homeBox"><a href="#"> Trang chủ </a></li>
           <li><a href="#"> Sự kiện</a></li>
           <li><a href="#"> Liên hệ</a></li>
         </ul>
       </div>
-      <div>
+
         <div class="Frame22">
           <div class="Group">
             <img src="images/Vector (Stroke) .png" class="Frame21">
@@ -650,29 +46,18 @@
             0123456789
           </div>
         </div>
-      </div>
+
     </div>
 
   </div>
 
-  </header>
+  </nav>
 
+      
+      <img src="/images/DamSen.png" class="DamSen">
+      <img src="/images/PARK.png" class="Park">
 
-
-
-  <main class="mainFrame">
-
-    <div class="mainBG">
-      <p class="DamSen"> Đầm Sen </p>
-      <p class="Park"> Park </p>
-
-      <div>
         <img src="images/image 2.png" class="image2">
-      </div>
-
-      <div>
-        <img src="images/Lisa_Arnold_Lay_Do_F2 3.png" class="Lisa">
-      </div>
 
       <div>
         <img src="images/balloon6.png" class="balloon6">
@@ -703,56 +88,88 @@
       </div>
 
       <div class="FrameTicket">
-        <p class="LoremParagraph">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac mollis justo. Etiam volutpat tellus quis risus volutpat, ut posuere ex facilisis. Suspendisse iaculis libero lobortis condimentum gravida. Aenean auctor iaculis risus, lobortis molestie lectus consequat a.
-        </p>
-        <div class="Frame28">
-          <p class="Textlorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-        <div class="Frame27">
-          <p class="Textlorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-        <div class="Frame26">
-          <p class="Textlorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-        <div class="Frame25">
-          <p class="Textlorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-        <div class="FrameDatve">
-          <p class="Datve"> Đặt vé </p>
-        </div>
-        <div class="FrameInfo">
-          <div>
-            <p class="Frame5"> Gói gia đình</p>
-          </div>
-          
-          <div>
-            <p class="Frame6"> Số lượng vé</p>
-          </div>
 
-          <div>
-            <p class="Frame7"> Ngày sử dụng</p>
-          </div>
-          
-          <div>
-            <p class="Frame8"> Họ và tên</p>
-          </div>
-          
-          <div>
-            <p class="Frame9"> Số điện thoại</p>
-          </div>
-          
-          <div>
-            <p class="Frame10"> Địa chỉ email</p>
-          </div>
-          
+        <img src="/images/Group.png" class="GroupForm1">
+
+                  <p class="LoremParagraph">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac mollis justo. Etiam volutpat tellus quis risus volutpat, ut posuere ex facilisis. Suspendisse iaculis libero lobortis condimentum gravida. Aenean auctor iaculis risus, lobortis molestie lectus consequat a.
+                  </p>
+                  <div class="Frame28">
+                    <img src="/images/star.png">
+                    <p class="Textlorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  </div>
+                  <div class="Frame27">
+                    <img src="/images/star.png">
+                    <p class="Textlorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  </div>
+                  <div class="Frame26">
+                    <img src="/images/star.png">
+                    <p class="Textlorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  </div>
+                  <div class="Frame25">
+                    <img src="/images/star.png">
+                    <p class="Textlorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  </div>
+
+                <img src="/images/Vector link.png" class="VectorLink">
+
+          <div class="">
+
+          <img src="/images/Group 2.png" class="GroupForm2">
+
+              <div class="FrameDatve">
+                <img src="/images/redbox.png" class="redbox">
+                <p class="Datve"> Đặt vé </p>
+              </div>
+
+                  <div class="FrameInfo">
+                    <div class="Yellow1">
+                      <img src="/images/Yellow1.png" class="YellowBox">
+                      <img src="/images/Arrow - Down 2.png" class="DownArrow">
+                    </div>
+
+                    <div class="Yellow2">
+                      <img src="/images/Yellow2.png" class="YellowBox">
+                      <img src="/images/calendar.png" class = "Calendar">
+                    </div>
+
+                    <div>
+                      <input class="Frame5" placeholder=" Gói gia đình"> 
+                    </div>
+                    
+                    <div>
+                      <input class="Frame6" placeholder="Số lượng vé">
+                    </div>
+
+                    <div>
+                      <input class="Frame7" placeholder=" Ngày sử dụng" >
+                    </div>
+                    
+                    <div>
+                      <input class="Frame8" placeholder="Họ và tên">
+                    </div>
+                    
+                    <div>
+                      <input class="Frame9" placeholder = "Số điện thoại">
+                    </div>
+                    
+                    <div>
+                      <input class="Frame10" placeholder = "Địa chỉ email" >
+                    </div>
+                 </div> 
           
         </div>
-      </div>
+
+      <img src="images/Lisa_Arnold_Lay_Do_F2 3.png" class="Lisa"> 
 
     </div>
+
+    <div class="group1">
+      <img src="/images/VeCuaBan.png" class="VeCuaBan">
+    </div>
+
       
-  </main>
+  </div>
 
 
 </body>
